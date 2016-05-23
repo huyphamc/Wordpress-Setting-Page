b07e1a5d7a579b260e26c0f8775a203025d1dfee
Huy Pham <huyphamc@gmail.com>
2016-05-23 23:04:10 +0700

    Init.

---
+++ wordpress-settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Wordpress Settings
+ * Plugin URI: http://huypham.io
+ * Description: Create Setting Page for Wordpress with Tabs
+ * Version: 1.0.0
+ * Author: Huy Pham
+ * Author URI: http://huypham.io
+ * License: GPL2
+ */
